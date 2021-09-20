e59cce84162958c10f8b53624e8495c0ee5363c6
CarlosMontesdeoca <mateo.game@hotmail.com>
2021-09-20 08:56:15 -0500

    boton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import axios from 'axios';
+import "bootswatch/dist/lux/bootstrap.min.css";
 import './App.css';
+
+const stripePromise = loadStripe("pk_test_51JbdbNA38POr8zD9O7Kepgp0aywqhYaTYZlEtXiFRtalsesEPFWNoWDKatGqccKYIM8dZyqXFU4EFwai8qy2Gd0z00RLMwwc25")
+
+const CheckoutForm = () => {
+
+  const stripe = useStripe();
+  const element = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: element.getElement(CardElement)
+    });
+
+    if (!error) {
+      console.log(paymentMethod)
+
+      const { id } = paymentMethod;
+
+      const { data } = await axios.post('http://localhost:3001/api/checkout', {
+        id,
+        amount: 10000
+      });
+
+      console.log(data);
+
+      element.getElement(CardElement).clear();
+    }
+  }
+
+  return <form onSubmit={handleSubmit} className="card card-body">
+    <img 
+      src="https://www.autobild.es/sites/autobild.es/public/dc/fotos/Lamborghini_Huracan_STO09_0.jpg" 
+      alt="lanborgini" 
+      className="img-fluid"
+    />
+
+    <h3 className="text-center my-2"> precio: 100$</h3>
+
+    <div className="form-group">
+      <CardElement className="form-control"/>
+    </div>
+    <button className="btn btn-success">
+      Comprar
+    </button>
+  </form>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-4.offset-md-4">
+            <CheckoutForm/>
+          </div>
+        </div>
+      </div>
+    </Elements>
   );
 }
 
